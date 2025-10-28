a77ad6fc7b72ca8f81f30b959e7a0b773fae420e
Alexx108 <alexx1088@gmail.com>
2025-10-28 20:10:25 +0500

    add city_controller

--- internal/controller/city_controller.go
+++ internal/controller/city_controller.go
@@ -2,9 +2,11 @@ package controller
 
 import (
 	"encoding/json"
+	"github.com/go-chi/chi/v5"
 	"location-service/internal/model"
 	"location-service/internal/service"
 	"net/http"
+	"strconv"
 )
 
 type CityController struct {
@@ -15,7 +17,7 @@ func NewCityController(s *service.CityService) *CityController {
 	return &CityController{service: s}
 }
 
-func (c *CityController) Create(w http.ResponseWriter, r *http.Request) {
+func (c *CityController) CreateCity(w http.ResponseWriter, r *http.Request) {
 	var city model.City
 	if err := json.NewDecoder(r.Body).Decode(&city); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -29,7 +31,7 @@ func (c *CityController) Create(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(city)
 }
 
-func (c *CityController) List(w http.ResponseWriter, r *http.Request) {
+func (c *CityController) ListCities(w http.ResponseWriter, r *http.Request) {
 	cities, err := c.service.ListCities(r.Context())
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -38,6 +40,59 @@ func (c *CityController) List(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(cities)
 }
 
-func (c *CityController) Get(w http.ResponseWriter, r *http.Request) {
+func (c *CityController) GetCity(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
+	city, err := c.service.GetCity(r.Context(), id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
 
+	json.NewEncoder(w).Encode(city)
+}
+
+// ✅ PUT /cities/{id}
+func (c *CityController) UpdateCity(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
+
+	var city model.City
+	if err := json.NewDecoder(r.Body).Decode(&city); err != nil {
+		http.Error(w, "invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	city.Id = int64(id)
+	if err := c.service.UpdateCity(r.Context(), &city); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(city)
+}
+
+// ✅ DELETE /cities/{id}
+func (c *CityController) DeleteCity(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		http.Error(w, "invalid id", http.StatusBadRequest)
+		return
+	}
+
+	if err := c.service.DeleteCity(r.Context(), id); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
