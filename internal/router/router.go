210da9e10e191bca6c9d1f1fc6e285aec6960e01
Alexx108 <alexx1088@gmail.com>
2025-11-09 20:02:47 +0500

    update router.go

--- internal/router/router.go
+++ internal/router/router.go
@@ -4,7 +4,6 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"location-service/internal/controller"
 	"location-service/internal/repository"
 	"location-service/internal/service"
 )
@@ -15,44 +14,17 @@ func NewRouter(pool *pgxpool.Pool) *chi.Mux {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	// ---- Cities ----
 	cityRepo := repository.NewCityRepository(pool)
 	cityService := service.NewCityService(cityRepo)
-	cityController := controller.NewCityController(cityService)
+	r.Mount("/cities", CityRoutes(cityService))
 
-	r.Route("/cities", func(r chi.Router) {
-		r.Get("/", cityController.ListCities)
-		r.Post("/", cityController.CreateCity)
-		r.Get("/{id}", cityController.GetCity)
-		r.Put("/{id}", cityController.UpdateCity)
-		r.Delete("/{id}", cityController.DeleteCity)
-	})
-
-	// ---- Streets ----
 	streetRepo := repository.NewStreetRepository(pool)
 	streetService := service.NewStreetService(streetRepo)
-	streetController := controller.NewStreetController(streetService)
+	r.Mount("/streets", StreetRoutes(streetService))
 
-	r.Route("/streets", func(r chi.Router) {
-		r.Get("/", streetController.ListStreets)
-		r.Post("/", streetController.CreateStreet)
-		r.Get("/{id}", streetController.GetStreet)
-		r.Put("/{id}", streetController.UpdateStreet)
-		r.Delete("/{id}", streetController.DeleteStreet)
-	})
-
-	// ----- Crossroads
 	crossroadRepo := repository.NewCrossroadRepository(pool)
 	crossroadService := service.NewCrossroadService(crossroadRepo)
-	crossroadController := controller.NewCrossroadController(crossroadService)
-
-	r.Route("/crossroads", func(r chi.Router) {
-		r.Get("/", crossroadController.ListCrossroads)
-		r.Post("/", crossroadController.CreateCrossroad)
-		r.Put("/{id}", crossroadController.UpdateCrossroad)
-		r.Delete("/{id}", crossroadController.DeleteCrossroad)
-		r.Get("/{id}", crossroadController.GetCrossroad)
-	})
+	r.Mount("/crossroads", CrossroadRoutes(crossroadService))
 
 	return r
 }
